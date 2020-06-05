0a9103fb7e0595fe21a38cfe9c136234588a8e0b
GStudent1 <1035193656@qq.com>
2020-06-05 17:09:51 +0800

    test

---
+++ demo.py
@@ -0,0 +1,24 @@
+#! /user/bin/python3
+#-*- coding:UTF-8 -*-
+
+
+# num_input1=0;
+# for num_input1<=1000:
+#     # num_input1=input("请输入一个数字");
+#     num_input=str(num_input1);
+#     num_list=list(num_input);
+#     lent = len(num_list);
+#     xxx=0;
+#     for x in num_list:
+#         xx=int(x);
+#         xxx+=xx**lent;
+#     if(xxx==num_input1):
+#         print(num_input1,end=" ");
+#     else:
+#         pass;
+#     num_input1 += 1;
+
+
+
+print(min(3,6));
+
